ca362d6bd4775432d2cac8c9b3c36ce8d90c8be0
Kinetix <x86app@126.com>
2021-05-16 15:48:30 +0800

    实现 Countdown 输入框 启停样式变化

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,14 +1,27 @@
 <template>
   <input id="countdown"
-    :style="{ display: !$store.state.onClock?'block':'none', 'background-color': countdownValue?'#fafafa':'#ef5350', 'color': countdownValue?'#212121':'#fafafa' }"
+    :style="{
+      display: !$store.state.onClock ? 'block' : 'none',
+      'background-color': backgroundColor,
+      color: color
+    }"
     @input="countdownCheckInput"
+    :disabled="countdownStarted"
     type="text" v-model="countdownInput" placeholder="00:00:00">
+  <button id="countdownControl" @click="countdownToggle()">
+    <span class="material-icons">{{ !countdownStarted ? 'play_arrow' : 'pause' }}</span>
+  </button>
 </template>
 <style>
 #countdown {
   border: none;
-  font-size: 9rem;
-  width: 40rem;
+  font-size: 10rem;
+  width: 48rem;
+  text-align: center;
+}
+#countdown:disabled {
+  background-color: #212121 !important;
+  color: #fafafa !important;
 }
 </style>
 <script>
@@ -17,15 +30,35 @@ export default {
   data() {
     return {
       countdownInput: '00:00:00',
-      countdownValue: '00:00:00'
+      countdownValue: '00:00:00',
+      countdownPreviousValue: '00:00:00',
+      countdownStarted: false,
+      backgroundColor: '#fafafa',
+      color: '#212121'
     }
   },
   methods: {
     countdownCheckInput() {
       console.log(this.countdownInput.match(/^[\d+]+:[0-5]?[0-9]:[0-5]?[0-9]$/));
-      if (this.countdownInput === '') return (this.countdownValue = '00:00:00')
-      else if (this.countdownInput.match(/^[\d+]+:[0-5]?[0-9]:[0-5]?[0-9]$/)) return (this.countdownValue = this.countdownInput)
-      else return (this.countdownValue = false)
+      if (this.countdownInput === '') {
+        this.backgroundColor = '#fafafa'
+        this.color = '#212121'
+        return (this.countdownValue = '00:00:00')
+      } else if (this.countdownInput.match(/^[\d+]+:[0-5]?[0-9]:[0-5]?[0-9]$/)) {
+        this.backgroundColor = '00:00:00'
+        this.color = '#212121'
+        return (this.countdownValue = this.countdownInput)
+      } else {
+        this.color = '#fafafa'
+        this.backgroundColor = '#ef5350'
+        return this.countdownValue = false
+      }
+    },
+    countdownStart() {
+
+    },
+    countdownToggle() {
+      this.countdownStarted = !this.countdownStarted
     }
   }
 }
